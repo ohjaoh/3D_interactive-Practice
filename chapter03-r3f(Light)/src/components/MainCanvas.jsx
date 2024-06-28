25c69b036d0afc707293ec4e61b0092ec8880029
ohjaoh <ohja1004@naver.com>
2024-06-28 16:09:50 +0900

    [오자현] R3F Light

---
+++ chapter03-r3f(Light)/src/components/MainCanvas.jsx
@@ -0,0 +1,28 @@
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Color } from "three";
+import { Meshes } from "./Meshes";
+import { Lights } from "./Lights";
+
+export const MainCanvas = () => {
+	return (
+		<Canvas
+			gl={{ antialias: true }}
+			shadows={"soft"}
+			camera={{
+				fov: 60,
+				aspect: window.innerWidth / window.innerHeight,
+				near: 0.1,
+				far: 100,
+				position: [5, 5, 5],
+			}}
+			scene={{
+				background: new Color(0x000000)
+			}}
+		>
+			<OrbitControls />
+			<Lights/>
+			<Meshes />
+
+		</Canvas>);
+};
---
+++ chapter03-r3f(Light)/src/components/Meshes.jsx
@@ -0,0 +1,23 @@
+import { Plane, TorusKnot } from "@react-three/drei";
+import * as THREE from "three";
+
+export const Meshes = () => {
+  return (
+    <>
+            <Plane args={[40, 40]} rotation-x={-Math.PI / 2}
+        receiveShadow>
+        <meshStandardMaterial />
+      </Plane>
+      <TorusKnot args={[1, 0.2, 128, 128, 2, 3]}
+
+        position={[-3, 1.6, 0]}
+        material-color={'teal'}
+        castShadow
+        receiveShadow>
+        <meshStandardMaterial
+          color={0xffffff}
+        />
+      </TorusKnot>
+    </>
+  );
+}
